168e52e620fb6b77ed3a70c41c19b210c06c0a05
Putra Iskandar <putraiskandarmuda78@gmail.com>
2022-06-27 22:50:39 +0700

    Fragment

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import Form from './components/Form';
-import LifecycleA from './components/LifecycleA';
+import FragmentDemo from './components/FragmentDemo';
+import Table from './components/Table';
+
 
 
 function App() {
   return (
     <div className="App">
-      <LifecycleA />
+      <Table />
+      {/* <FragmentDemo /> */}
     </div>
   );
 }
--- src/components/LifecycleA.js
+++ src/components/LifecycleA.js
@@ -1,36 +1,55 @@
-import React, { Component } from 'react'
-import LifecycleB from './LifecycleB'
-
+import React, { Component } from 'react';
+import LifecycleB from './LifecycleB';
 
 class LifecycleA extends Component {
-  
   constructor(props) {
-    super(props)
-  
+    super(props);
+
     this.state = {
-       name: 'Putra'
-    }
-    console.log('LifecycleA constructor')
+      name: 'Putra',
+    };
+    console.log('LifecycleA constructor');
   }
 
-  static getDerivedStateFromProps(props, state){
-    console.log('LifecycleA getDerivedStateFromProps')
-    return null
+  static getDerivedStateFromProps(props, state) {
+    console.log('LifecycleA getDerivedStateFromProps');
+    return null;
   }
 
-  componentDidMount(){
-    console.log('LifecycleA componentDidMount')
+  componentDidMount() {
+    console.log('LifecycleA componentDidMount');
   }
-   
+
+  shouldComponentUpdate() {
+    console.log('LifecycleA shouldComponentUpdate');
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log('LifecycleA getSnapshotBeforeUpdate');
+    return null;
+  }
+
+  componentDidUpdate() {
+    console.log('LifecycleA componentDidUpdate');
+  }
+
+  changeState = () => {
+    this.setState({
+      name: 'Codevolution',
+    });
+  };
+
   render() {
-    console.log('LifecycleA render')
+    console.log('LifecycleA render');
     return (
       <div>
         <div>Lifecycle A</div>
+        <button onClick={this.changeState}>Change state</button>
         <LifecycleB />
       </div>
-      )
+    );
   }
 }
 
-export default LifecycleA
+export default LifecycleA;
