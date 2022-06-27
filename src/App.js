bcd7c188c7562ff902ba99bafe99249cad2fe80c
Putra Iskandar <putraiskandarmuda78@gmail.com>
2022-06-27 16:44:29 +0700

    styling and css basics

--- src/App.js
+++ src/App.js
@@ -11,11 +11,19 @@ import EventBind from './components/EventBind';
 import ParentComponent from './components/ParentComponent';
 import UserGreeting from './components/UserGreeting';
 import NameList from './components/NameList';
+import Stylesheet from './components/Stylesheet';
+import Inline from './components/Inline';
+import './appStyles.css'
+import styles from './appStyles.module.css'
 
 function App() {
   return (
     <div className="App">
-      <NameList />
+      {/* <Inline /> */}
+      <h1 className='error'>Error</h1>
+      <h1 className={styles.success}>Success</h1>
+      {/* <Stylesheet primary={true} /> */}
+      {/* <NameList /> */}
       {/* <UserGreeting /> */}
       {/* <ParentComponent /> */}
       {/* <EventBind /> */}
---
+++ src/components/Inline.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const heading = {
+  fontSize: '72px', 
+  color: 'blue'
+}
+
+
+function Inline() {
+  return (
+    <h1 style={heading}>Inline</h1>
+  )
+}
+
+export default Inline
