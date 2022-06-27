427db2418508df3ddbba5ce9e47072b2f8d089a0
Putra Iskandar <putraiskandarmuda78@gmail.com>
2022-06-27 13:47:18 +0700

    Method as props

--- src/App.js
+++ src/App.js
@@ -4,21 +4,31 @@ import Greet from './components/Greet';
 import Welcome from './components/Welcome';
 import Hello from './components/Hello';
 import Message from './components/Message';
+import Counter from './components/Counter';
+import FunctionClick from './components/FunctionClick';
+import ClassClick from './components/ClassClick';
+import EventBind from './components/EventBind';
+import ParentComponent from './components/ParentComponent';
 
 function App() {
   return (
     <div className="App">
-      <Message />
+      <ParentComponent />
+      {/* <EventBind /> */}
+      {/* <FunctionClick /> */}
+      {/* <ClassClick /> */}
+      {/* <Counter /> */}
+      {/* <Message /> */}
       {/* <Greet name='Bruce' heroName='Batman'>
         <p>This is children props</p>  
-      </Greet>
-      <Greet name='Clark' heroName='Superman'>
+      </Greet> */}
+      {/* <Greet name='Clark' heroName='Superman'>
         <button>Action</button>
-      </Greet>  
-      <Greet name='Diana' heroName='Wonder Woman'/>
-      <Welcome name='Bruce' heroName='Batman'/>
-      <Welcome name='Clark' heroName='Superman'/>
-      <Welcome name='Diana' heroName='Wonder Woman'/> */}
+      </Greet>   */}
+      {/* <Greet name='Diana' heroName='Wonder Woman'/> */}
+      {/* <Welcome name='Bruce' heroName='Batman'/> */}
+      {/* <Welcome name='Clark' heroName='Superman'/> */}
+      {/* <Welcome name='Diana' heroName='Wonder Woman'/> */}
       {/* <Hello/> */}
     </div>
   );
