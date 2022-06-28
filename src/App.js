f77ef687074dba5753fdae3ba1c9769f885869a1
Putra Iskandar <putraiskandarmuda78@gmail.com>
2022-06-28 16:42:38 +0700

    Error Boundary

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import FragmentDemo from './components/FragmentDemo';
-import Table from './components/Table';
-import PureComp from './components/PureComp';
-import ParentComp from './components/ParentComp';
-import RefsDemo from './components/RefsDemo';
-import FocusInput from './components/FocusInput';
-import FRParentInput from './components/FRParentInput';
-import PortalDemo from './components/PortalDemo';
+import Hero from './components/Hero';
+import ErrorBoundary from './components/ErrorBoundary';
+
 
 
 
 function App() {
   return (
     <div className="App">
-      <PortalDemo />
-      {/* <FRParentInput /> */}
-      {/* <RefsDemo /> */}
-      {/* <FocusInput /> */}
+      <ErrorBoundary>
+        <Hero heroName='Batman'/>
+      </ErrorBoundary>
+      
+      <ErrorBoundary>
+        <Hero heroName='Superman'/>
+      </ErrorBoundary>
+      
+      <ErrorBoundary>
+        <Hero heroName='Joker'/>
+      </ErrorBoundary>
     </div>
   );
 }
